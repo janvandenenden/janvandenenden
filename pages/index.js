0ca98dfee1a5c01b166162b7cbbb5bb4654897ea
Jan Van den Enden <jan@enden.be>
2023-03-22 09:31:57 +0100

    add resume to home page

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,10 @@ import Head from "next/head";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { MdOutlineMail } from "react-icons/md";
-import { DiGithubAlt } from "react-icons/di";
-import { FaLinkedinIn } from "react-icons/fa";
-import { AiFillEye } from "react-icons/ai";
+import { HiViewGrid } from "react-icons/hi";
+import { HiDocumentArrowDown } from "react-icons/hi2";
 import Link from "next/link";
 import useWindowSize from "../hooks/useWindowSize";
-import useDarkMode from "../hooks/useDarkMode";
-import Nav from "../components/Nav";
 
 export default function Home() {
   const size = useWindowSize();
@@ -18,7 +15,7 @@ export default function Home() {
         <title>Jan Van den Enden | Digital Developer</title>
         <meta
           name="description"
-          content="Hi! I'm Jan, a social scientist turned developer. I'm experienced in building customer-facing SaaS, websites and setting up lean business operations."
+          content="Hi! I'm Jan, a front-end developer based in Brussels, Belgium. I love to code creative and functional websites that scale and help projects go from zero to one using lean strategies."
         />
       </Head>
 
@@ -30,7 +27,7 @@ export default function Home() {
       >
         <div className="h-full my-auto">
           <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
-            <div className="w-full flex flex-col my-auto order-last lg:order-first lg:col-span-7 overflow-hidden ">
+            <div className="w-full flex flex-col my-auto order-last lg:order-first lg:col-span-7">
               <motion.h1
                 initial={{ x: "-100vw" }}
                 animate={{ x: 0 }}
@@ -51,11 +48,11 @@ export default function Home() {
                 transition={{ duration: 1, type: "tween", delay: 1 }}
                 exit={{ opacity: 0 }}
               >
-                <p className="text-lg md:text-2xl font-light mb-3">
+                <p className="text-lg md:text-xl font-light mb-3">
                   I love to code creative and functional websites that scale and
                   help projects go from zero to one using lean strategies.
                 </p>
-                <p className="text-lg md:text-2xl font-light mb-3">
+                <p className="text-lg md:text-xl font-light mb-3">
                   Get in touch if you have a project in mind or would like to
                   discuss potential opportunities.
                 </p>
@@ -65,33 +62,45 @@ export default function Home() {
                     whileHover={{ scale: 1.05 }}
                     initial={{ scale: 1 }}
                     whileTap={{ scale: 0.95 }}
-                    href="mailto:jan@enden.be"
-                    rel="noreferrer"
+                    href="/resume.pdf"
                     target="_blank"
-                    className="bg-white dark:bg-indigo-600 hover:text-indigo-700 hover:dark:text-white hover:dark:bg-indigo-500 shadow-xl flex items-center text-xl py-2 cursor-pointer dark:text-white px-6 md:mr-6 mb-3 md:mb-0 rounded font-extrabold justify-center"
+                    rel="noreferrer"
+                    className="bg-white  hover:text-indigo-500 dark:text-indigo-600 shadow flex items-center text-xl py-2 cursor-pointer px-6 md:mr-6 mb-3 md:mb-0 rounded font-extrabold justify-center"
                   >
-                    <MdOutlineMail className="text-2xl  mr-3 dark:hover:text-white" />
-                    Let&apos;s talk
+                    <HiDocumentArrowDown className="text-2xl mr-3" />
+                    Resume
                   </motion.a>
                   <motion.div
                     whileHover={{ scale: 1.05 }}
                     initial={{ scale: 1 }}
                     whileTap={{ scale: 0.95 }}
-                    className="bg-white dark:bg-indigo-600 hover:text-indigo-700 hover:dark:text-white hover:dark:bg-indigo-500 shadow-xl text-xl py-2 cursor-pointer dark:text-white px-6 md:mr-6 mb-3 md:mb-0 rounded font-extrabold"
+                    className="bg-white  hover:text-indigo-500 dark:text-indigo-600 shadow text-xl py-2 cursor-pointer px-6 md:mr-6 mb-3 md:mb-0 rounded font-extrabold"
                   >
                     <Link
                       href="/projects"
                       className="flex items-center justify-center "
                     >
-                      <AiFillEye className="text-2xl  mr-3 dark:hover:text-white" />
-                      See projects
+                      <HiViewGrid className="text-2xl mr-3" />
+                      Projects
                     </Link>
                   </motion.div>
+                  <motion.a
+                    whileHover={{ scale: 1.05 }}
+                    initial={{ scale: 1 }}
+                    whileTap={{ scale: 0.95 }}
+                    href="mailto:jan@enden.be"
+                    rel="noreferrer"
+                    target="_blank"
+                    className="bg-indigo-600 text-white dark:text-white  hover:dark:text-white hover:dark:bg-indigo-500 shadow flex items-center text-xl py-2 cursor-pointer px-6 md:mr-6 mb-3 md:mb-0 rounded font-extrabold justify-center"
+                  >
+                    <MdOutlineMail className="text-2xl  mr-3 dark:hover:text-white" />
+                    Let&apos;s talk
+                  </motion.a>
                 </div>
               </motion.div>
             </div>
             <motion.div
-              className="order-first lg:order-last px-16 hidden md:px-36 lg:px-0 lg:grid lg:col-span-5"
+              className="order-first lg:order-last px-16 hidden lg:px-0 lg:grid lg:col-span-5"
               whileInView={{ scale: 1 }}
               initial={{ scale: 0.9 }}
               transition={{ delay: 2.5 }}
@@ -103,7 +112,7 @@ export default function Home() {
                 height="300"
                 priority
                 sizes="100vw"
-                className="filter dark:hue-rotate-180 contrast-100 opacity-100 rounded-2xl"
+                className="opacity-75 filter grayscale"
                 style={{
                   width: "100%",
                   height: "auto",
